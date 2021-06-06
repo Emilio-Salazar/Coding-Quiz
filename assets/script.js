1024c0c34673aa50ae25cd68c92698bad4deb095
E Lazar <emiliosalazar4@gmail.com>
2021-06-06 16:52:56 -0500

    initial html and styling

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var question1 = {
+    question: "This file type contains most of the text content of a website.",
+    answerA:"css",
+    answerB:"html",
+    answerC:"javascript",
+    answerD:"flexbox",
+    correctAnswer: "html"
+}
+var starQuiz = function() {
+    alert("Welcome to the coding quiz!")
+    
+}
